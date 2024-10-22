17910921574c6718be36b5d7afbb16878c4022db
Antoine PLIN <antoine.plin.cjd@gmail.com>
2024-10-22 11:05:01 +0200

    Added example usage to main.rs

--- src/gpio.rs
+++ src/gpio.rs
@@ -2,8 +2,8 @@
 #![no_main]
 use core::ptr::{read_volatile, write_volatile};
 
-#[cfg(target_arch = "avr")]
-mod atmega328 {
+/* #[cfg(target_arch = "avr")]
+mod atmega328 { */
     pub const DDRB: *mut u8 = 0x24 as *mut u8;
     pub const PORTB: *mut u8 = 0x25 as *mut u8;
     pub const PINB: *mut u8 = 0x23 as *mut u8;
@@ -15,16 +15,16 @@ mod atmega328 {
     pub const DDRD: *mut u8 = 0x2A as *mut u8;
     pub const PORTD: *mut u8 = 0x2B as *mut u8;
     pub const PIND: *mut u8 = 0x29 as *mut u8;
-}
+/* } */
 
-#[cfg(target_arch = "xtensa")]
+/* #[cfg(target_arch = "xtensa")]
 mod esp8266 {
     pub const GPIO_ENABLE_W1TS: *mut u32 = 0x60000310 as *mut u32;
     pub const GPIO_ENABLE_W1TC: *mut u32 = 0x60000314 as *mut u32;
     pub const GPIO_OUT_W1TS: *mut u32 = 0x60000304 as *mut u32;
     pub const GPIO_OUT_W1TC: *mut u32 = 0x60000308 as *mut u32;
     pub const GPIO_IN: *mut u32 = 0x60000318 as *mut u32;
-}
+} */
 
 #[repr(u8)]
 pub enum Gpio_Mode {
@@ -38,34 +38,65 @@ pub enum Gpio_Level {
     High = 1,
 }
 
+pub enum Atmega_Port {
+    PortB,
+    PortC,
+    PortD,
+}
+
 
 pub struct Gpio {
     pin: u8,
+    pin_reg : *mut u8,
+    ddr_reg : *mut u8,
+    port_reg : *mut u8,
+    
 }
 
 impl Gpio {
-    pub fn new(pin: u8) -> Self {
-        Gpio { pin }
+    pub fn new(pin: u8, port : Atmega_Port) -> Self {
+        let mut pin_reg: *mut u8;
+        let mut ddr_reg: *mut u8;
+        let mut port_reg: *mut u8;
+
+        match port{
+            Atmega_Port::PortB => {
+                pin_reg = PINB;
+                ddr_reg = DDRB;
+                port_reg = PORTB;
+            },
+            Atmega_Port::PortC => {
+                pin_reg = PINC;
+                ddr_reg = DDRC;
+                port_reg = PORTC;
+            },
+            Atmega_Port::PortD => {
+                pin_reg = PIND;
+                ddr_reg = DDRD;
+                port_reg = PORTD;
+            },
+        }
+        Gpio { pin, pin_reg, ddr_reg, port_reg }
     }
 
     pub fn set_mode(&self, mode: Gpio_Mode) {
         unsafe {
             #[cfg(target_arch = "avr")]
             {
-                let ddrb = read_volatile(DDRB);
+                let ddr = read_volatile(self.ddr_reg);
                 match mode {
-                    Gpio_Mode::Output => write_volatile(DDRB, ddrb | (1 << self.pin)),
-                    Gpio_Mode::Input => write_volatile(DDRB, ddrb & !(1 << self.pin)),
+                    Gpio_Mode::Output => write_volatile(self.ddr_reg, ddr | (1 << self.pin)),
+                    Gpio_Mode::Input => write_volatile(self.ddr_reg, ddr & !(1 << self.pin)),
                 }
             }
 
-            #[cfg(target_arch = "xtensa")]
+            /* #[cfg(target_arch = "xtensa")]
             {
                 match mode {
                     Gpio_Mode::Output => write_volatile(GPIO_ENABLE_W1TS, 1 << self.pin),
                     Gpio_Mode::Input => write_volatile(GPIO_ENABLE_W1TC, 1 << self.pin),
                 }
-            }
+            } */
         }
     }
 
@@ -73,26 +104,20 @@ impl Gpio {
         unsafe {
             #[cfg(target_arch = "avr")]
             {
-                let portb = read_volatile(PORTB);
+                let portb = read_volatile(self.port_reg);
                 match level {
-                    Gpio_Level::High => write_volatile(PORTB, portb | (1 << self.pin)),
-                    Gpio_Level::Low => write_volatile(PORTB, portb & !(1 << self.pin)),
+                    Gpio_Level::High => write_volatile(self.port_reg, portb | (1 << self.pin)),
+                    Gpio_Level::Low => write_volatile(self.port_reg, portb & !(1 << self.pin)),
                 }
             }
 
-            #[cfg(target_arch = "xtensa")]
+            /* #[cfg(target_arch = "xtensa")]
             {
                 match level {
                     Gpio_Level::High => write_volatile(GPIO_OUT_W1TS, 1 << self.pin),
                     Gpio_Level::Low => write_volatile(GPIO_OUT_W1TC, 1 << self.pin),
                 }
-            }
+            } */
         }
     }
-}
-
-fn main() {
-    let gpio = Gpio::new(3);
-    gpio.set_mode(Gpio_Mode::Output);
-    gpio.set_level(Gpio_Level::High);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,22 @@
 #![no_main]
 use core::panic::PanicInfo;
 use cortex_m_rt::entry;
+mod gpio;
+use gpio::*;
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    let gpio = Gpio::new(5, Atmega_Port::PortB);
+    gpio.set_mode(Gpio_Mode::Output);
+    gpio.set_level(Gpio_Level::High);
+    loop {
+        for _ in 0..400000{
+            gpio.set_level(Gpio_Level::High);
+        }
+        for _ in 0..400000{
+            gpio.set_level(Gpio_Level::Low);
+        }
+    }
 }
 
 #[panic_handler]
