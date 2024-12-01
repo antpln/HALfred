cacb58244d504729603d6bf7ec5a4f2aea29b93e
Antoine <38262807+antpln@users.noreply.github.com>
2024-12-01 17:00:32 +0100

    Hifive1 (#1)
    * Update Cargo.toml
    
    * Update main.rs
    
    * Update gpio.rs
    
    * Add files via upload
    
    * USART HIFIVE 1
    
    ---------
    
    Co-authored-by: romtvl <tvlromain@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 #![no_std]
 #![no_main]
+
 use core::panic::PanicInfo;
-use cortex_m_rt::entry;
-use usart::Usart;
+
 mod usart;
+use usart::Usart;
 
-mod gpio;
 
-#[entry]
-fn main() -> ! {
-    let mut usart = Usart::new();
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    let usart = Usart::new();
+
     usart.init(9600);
 
+
     loop {
-        let message = b"Hello, USART! \n";
-        for &byte in message {
-            usart.transmit(byte);
+        // Envoi de "Hello World\n" caractère par caractère
+        let message = b"Hello World\n";
+        for &c in message {
+            usart.transmit(c);
+        }
+    
+        // Réception d'un caractère
+        let received = Usart.receive();
+        if received == b'H' {
+            // Répondre "i\n"
+            let response = b"i\n";
+            for &c in response {
+                usart.transmit(c);
+            }
         }
     }
 }
@@ -24,3 +37,9 @@ fn main() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+// cargo +nightly build -Z build-std=core --target avr-unknown-gnu-atmega328 --release
+// qemu-system-avr -M uno -bios halfred.elf -serial tcp::5678,server=on,wait=off
+
+// cargo +nightly build -Z build-std=core --target thumbv7m-none-eabi --release
+// qemu-system-arm -M lm3s6965evb -kernel halfred -serial tcp::5678,server=on,wait=off
